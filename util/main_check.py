9fca45a58eeef1d903f6a7ca0b73e052ad6d7f18
Nikolay Korolev <korolevns98@gmail.com>
2019-09-25 14:57:14 +0300

    Add simple check for users rights

---
+++ util/main_check.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import subprocess
+import os
+import re
+import sys
+import json
+
+
+user = os.environ.get('GITHUB_ACTOR', None)
+if not user:
+	print(f"Нету переменной среды 'GITHUB_ACTOR'")
+	sys.exit(1)
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(f'{directory}/users.json') as users:
+    users_to_rights = json.load(users)
+
+surname = users_to_rights.get(user, None)
+if not surname:
+	print(f"Пользователь не имеет прав писать в данный репозиторий")
+	sys.exit(1)
+
+re_task_files = re.compile(fr"task([1-6])/{surname}_\1_\d\.sql")
+
+file_diff = subprocess.check_output(["git", "diff", "origin/master", "--name-only"]).decode('utf-8').split('\n')
+file_diff = file_diff[:-1]
+
+for file in file_diff:
+	if not re_task_files.fullmatch(file):
+		print(f"Отсутствие прав на изменение '{file}'")
+		sys.exit(1)
