dfe1e4b29610bb832dd24fb0eca2c651e464f730
Ivan Saenko <tigersay@yandex.ru>
2019-09-26 23:08:01 +0300

    Create Saenko_1_1.sql

--- task1/Saenko_1_1.sql
+++ task1/Saenko_1_1.sql
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 SELECT customer_rk, 
 CASE
 WHEN middle_nm LIKE '%ИЧ' THEN 'M'
 WHEN middle_nm LIKE '%НА' THEN 'F'
 END AS gender 
 FROM cd_customers WHERE valid_to_dttm = '5999-01-01 00:00:00'
+=======
+SELECT customer_rk, 
+CASE
+WHEN middle_nm LIKE ('%ИЧ') THEN 'M'
+WHEN middle_nm LIKE ('%НА') THEN 'F'
+END AS gender 
+FROM srcdt.cd_customers WHERE valid_to_dttm = '5999-01-01 00:00:00'
+>>>>>>> Create Saenko_1_1.sql
