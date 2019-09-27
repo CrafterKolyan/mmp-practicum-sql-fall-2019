9b7cf355a120a5d2cdc08a170d94caa4f2360286
Nikolay Korolev <korolevns98@gmail.com>
2019-09-27 12:19:38 +0300

    Korolev: Solve 1

--- task1/Korolev_1_2.sql
+++ task1/Korolev_1_2.sql
@@ -5,5 +5,6 @@ FROM
 WHERE 
   valid_from_dttm <= '2014-01-01 00:00:00'
   AND valid_to_dttm >= '2014-01-01 00:00:00'
-ORDER BY monthly_income_amt DESC
+ORDER BY
+  monthly_income_amt DESC
 LIMIT 10
