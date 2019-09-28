a7fc9458cb44fae81f6dab12820606bcae9051c6
alexust98 <45120679+alexust98@users.noreply.github.com>
2019-09-28 20:10:41 +0300

    Прак (#27)
    * Add files via upload
    
    * Update Ustyuzhanin_1_1.sql
    
    * Update Ustyuzhanin_1_1.sql
    
    * Update Ustyuzhanin_1_1.sql

---
+++ task1/Ustyuzhanin_1_1.sql
@@ -0,0 +1,10 @@
+SELECT 
+    customer_rk,
+    CASE
+        WHEN middle_nm LIKE "%НА" THEN 'M'
+        WHEN middle_nm LIKE "%ИЧ" THEN 'F'
+        ELSE NULL
+    END as gender
+FROM srcdt.cd_customers
+WHERE valid_to_dttm = '5999-01-01 00:00:00'
+
