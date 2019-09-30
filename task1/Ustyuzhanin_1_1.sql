6f783906f348b5a915113df08164e26b9b2d3bb4
alexust98 <45120679+alexust98@users.noreply.github.com>
2019-10-01 00:35:43 +0300

    Update Ustyuzhanin_1_1.sql (#34)

--- task1/Ustyuzhanin_1_1.sql
+++ task1/Ustyuzhanin_1_1.sql
@@ -1,8 +1,8 @@
 SELECT 
     customer_rk,
     CASE
-        WHEN middle_nm LIKE "%НА" THEN 'M'
-        WHEN middle_nm LIKE "%ИЧ" THEN 'F'
+        WHEN middle_nm LIKE "%НА" THEN 'F'
+        WHEN middle_nm LIKE "%ИЧ" THEN 'M'
         ELSE NULL
     END as gender
 FROM srcdt.cd_customers
