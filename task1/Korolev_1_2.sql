41ec20a5528aea585fa6cb96430ff1ea8c7a1dea
Nikolay Korolev <korolevns98@gmail.com>
2019-09-30 22:51:05 +0300

    Change 1.2

--- task1/Korolev_1_2.sql
+++ task1/Korolev_1_2.sql
@@ -3,7 +3,7 @@ SELECT
 FROM
   cd_customers
 WHERE 
-  valid_from_dttm <= '2014-01-01 00:00:00'
+  valid_from_dttm < '2015-01-01 00:00:00'
   AND valid_to_dttm >= '2014-01-01 00:00:00'
 ORDER BY
   monthly_income_amt DESC
