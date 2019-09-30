b786cae77a057ac1d19dce0425d77ca6088256c4
alexust98 <45120679+alexust98@users.noreply.github.com>
2019-09-30 21:57:43 +0300

    Update Ustyuzhanin_1_2.sql

--- task1/Ustyuzhanin_1_2.sql
+++ task1/Ustyuzhanin_1_2.sql
@@ -5,7 +5,7 @@ FROM
 WHERE
 	valid_from_dttm < '2015-01-01 00:00:00'
     AND
-    valid_to_dttm > '2014-01-01 00:00:00'
+    	valid_to_dttm > '2014-01-01 00:00:00'
 ORDER BY
     monthly_income_amt DESC
 LIMIT 10
