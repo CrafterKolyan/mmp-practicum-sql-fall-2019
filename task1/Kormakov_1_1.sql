df935d9e1a2344856deee3d41768559ae926c82c
Georgiy Kormakov <33712171+mrph2898@users.noreply.github.com>
2019-10-01 19:01:20 +0300

    Update Kormakov_1_1.sql (#39)
    * Create Kormakov_1_2.sql
    
    * Update Kormakov_1_1.sql
    
    First task is completed

--- task1/Kormakov_1_1.sql
+++ task1/Kormakov_1_1.sql
@@ -1,1 +1,11 @@
-select
+SELECT 
+    customer_rk,
+    CASE
+        WHEN UPPER(middle_nm) LIKE '%ИЧ' THEN 'M'
+        WHEN UPPER(middle_nm) LIKE '%НА' THEN 'F'
+        ELSE NULL
+    END AS gender
+FROM
+    srcdt.cd_customers
+WHERE
+    valid_to_dttm = '5999-01-01 00:00:00';
