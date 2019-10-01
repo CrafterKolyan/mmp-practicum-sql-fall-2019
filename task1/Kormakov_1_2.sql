7d2107eecf032ec891703e7864192e10a6a52ca4
Georgiy Kormakov <33712171+mrph2898@users.noreply.github.com>
2019-10-01 22:47:29 +0300

    Update Kormakov_1_2.sql (#43)
    * Create Kormakov_1_2.sql
    
    * Update Kormakov_1_1.sql
    
    First task is completed
    
    * Update Kormakov_1_2.sql
    
    Second task is solved

--- task1/Kormakov_1_2.sql
+++ task1/Kormakov_1_2.sql
@@ -1,1 +1,9 @@
-SELECT first FROM db
+SELECT 
+    monthly_income_amt AS max10_income_in_2014
+FROM
+    srcdt.cd_customers
+WHERE
+    YEAR(valid_from_dttm) < 2015
+        AND YEAR(valid_to_dttm) >= 2014
+ORDER BY max10_income_in_2014 DESC
+LIMIT 10;
