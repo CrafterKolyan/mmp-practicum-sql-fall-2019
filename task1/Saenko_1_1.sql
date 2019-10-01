17af88d2ed930c197a7b3c2ebc348f26887d0c14
Ivan Saenko <tigersay@yandex.ru>
2019-10-01 21:39:28 +0300

    Add files via upload

--- task1/Saenko_1_1.sql
+++ task1/Saenko_1_1.sql
@@ -1,6 +1,6 @@
 SELECT customer_rk, 
 CASE
-WHEN middle_nm LIKE ('%ИЧ') THEN 'M'
-WHEN middle_nm LIKE ('%НА') THEN 'F'
+WHEN middle_nm LIKE '%ИЧ' THEN 'M'
+WHEN middle_nm LIKE '%НА' THEN 'F'
 END AS gender 
-FROM srcdt.cd_customers WHERE valid_to_dttm = '5999-01-01 00:00:00'
+FROM cd_customers WHERE valid_to_dttm = '5999-01-01 00:00:00'
