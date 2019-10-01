1a1669516f7b0768c33839f008f2d83ac2df103a
Alexander Chernyshev <32341040+sasasagagaga@users.noreply.github.com>
2019-10-01 20:25:19 +0300

    Task 01 solved (#40)

---
+++ task1/Chernyshev_1_1.sql
@@ -0,0 +1,10 @@
+SELECT
+    customer_rk,
+    (CASE
+        WHEN middle_nm LIKE '%Ч' THEN 'M'
+        WHEN middle_nm LIKE '%А' THEN 'F'
+    END) AS gender
+FROM
+    cd_customers
+WHERE
+    valid_to_dttm = '5999-01-01';
