adfa8e1f5bcf6d1db93956300bab39591054b0a4
Maksim Eremeev <maks5507@yandex.ru>
2019-10-02 15:24:31 +0300

    minor

--- task1/Eremeev_1_3.sql
+++ task1/Eremeev_1_3.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT 
-    concat(floor(YEAR(birth_dt) / 10), '0-е') AS Generation 
+    CONCAT(FLOOR(YEAR(birth_dt) / 10), '0-е') AS Generation 
 FROM cd_customers
 WHERE YEAR(valid_to_dttm) = 5999 
 ORDER BY Generation DESC 
