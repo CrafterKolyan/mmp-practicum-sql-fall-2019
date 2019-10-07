7b9833db4475ad0b837608cc1d3c3e82b005e23a
alexust98 <45120679+alexust98@users.noreply.github.com>
2019-10-08 00:51:05 +0300

    Add files via upload

---
+++ task2/Ustyuzhanin_2_2.sql
@@ -0,0 +1,19 @@
+SELECT
+	SUM(rk), dt
+FROM
+(
+	SELECT
+		count(DISTINCT customer_rk)as rk, YEAR(birth_dt) as dt
+	FROM
+		srcdt.cd_customers
+	WHERE
+		monthly_income_amt in (50000, 60000)
+	GROUP BY
+		customer_rk, birth_dt
+	HAVING 
+		count(DISTINCT monthly_income_amt) = 2
+	ORDER BY
+		dt
+	) as sql_kostil
+GROUP BY
+	dt
