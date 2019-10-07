fb006948cfbd058ae9264dab9f4f9cdc259ddb0d
Svetlana Kryzhanovskaya <47041067+kryzhanovskaya@users.noreply.github.com>
2019-10-07 23:06:56 +0300

    Update Kryzhanovskaya_2_2.sql

--- task2/Kryzhanovskaya_2_2.sql
+++ task2/Kryzhanovskaya_2_2.sql
@@ -1,6 +1,10 @@
-SELECT COUNT(customer_rk) - COUNT(DISTINCT customer_rk) AS cnt
-FROM(
-	SELECT DISTINCT customer_rk, monthly_income_amt
+SELECT COUNT(DISTINCT customer_rk) AS cnt_customers, YEAR(birth_dt) AS birth_year
+FROM cd_customers
+WHERE customer_rk IN (
+	SELECT customer_rk
 	FROM cd_customers
 	WHERE monthly_income_amt IN (50000, 60000) 
-) AS a
+	GROUP BY customer_rk
+	HAVING COUNT(DISTINCT monthly_income_amt) = 2
+)
+GROUP BY birth_year
