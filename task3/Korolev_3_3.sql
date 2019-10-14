1af9b84b4b379dd00abf4252477193181dfe36a1
Nikolay Korolev <korolevns98@gmail.com>
2019-10-15 02:40:37 +0300

    Solve 3.3

---
+++ task3/Korolev_3_3.sql
@@ -0,0 +1,46 @@
+SELECT DISTINCT
+  years.year_no,
+  COALESCE(deposits, 0) AS deposits_openings,
+  COALESCE(prolongations, 0) AS prolongations,
+  COALESCE(prolongations_with_50_percent_boost, 0) AS prolongations_with_50_percent_boost
+FROM (
+  SELECT DISTINCT
+    year_no
+  FROM
+    calendar
+  WHERE year_no >= YEAR((
+      SELECT
+        MIN(renewed_dt)
+      FROM
+        account_periods)
+    )) AS years
+  LEFT JOIN (
+    SELECT
+      YEAR(renewed_dt) AS year_no,
+      COUNT(*) AS deposits
+    FROM
+      account_periods
+    WHERE account_renewal_cnt = 1
+    GROUP BY year_no) AS deposits
+    ON years.year_no = deposits.year_no
+  LEFT JOIN (
+    SELECT
+      YEAR(renewed_dt) AS year_no,
+      COUNT(*) AS prolongations
+    FROM
+      account_periods
+    WHERE account_renewal_cnt > 1
+    GROUP BY year_no) AS prolongations
+    ON years.year_no = prolongations.year_no
+  LEFT JOIN (
+    SELECT
+      YEAR(ac2.renewed_dt) AS year_no,
+      COUNT(*) AS prolongations_with_50_percent_boost
+    FROM
+      account_periods AS ac1
+      JOIN account_periods AS ac2
+        ON ac1.account_rk = ac2.account_rk
+        AND ac1.account_renewal_cnt + 1 = ac2.account_renewal_cnt
+    WHERE ac1.opening_amt * 3 < 2 * ac2.opening_amt
+    GROUP BY year_no) AS prolongations_50
+    ON years.year_no = prolongations_50.year_no
