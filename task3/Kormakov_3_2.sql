ed2a5ee6bf3d346086145d02bd997dce5ddd533a
Georgiy Kormakov <33712171+mrph2898@users.noreply.github.com>
2019-10-15 15:47:03 +0300

    Update Kormakov_3_2.sql

--- task3/Kormakov_3_2.sql
+++ task3/Kormakov_3_2.sql
@@ -9,7 +9,7 @@ FROM
         srcdt.account_periods
     GROUP BY renewed_dt , expiration_dt) AS acc_per_nums 
     ON calendar.calendar_dt >= acc_per_nums.renewed_dt
-        AND calendar.calendar_dt <= acc_per_nums.expiration_dt
+        AND calendar.calendar_dt < acc_per_nums.expiration_dt
 	WHERE calendar.calendar_dt BETWEEN (SELECT 
             MIN(renewed_dt)
         FROM
