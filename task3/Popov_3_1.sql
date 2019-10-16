0481739182713ca8beee93acc1d11264ec679ff7
aredosbyk <43991422+aredosbyk@users.noreply.github.com>
2019-10-16 16:31:43 +0300

    new solutions + update (#124)
    * Add files via upload
    
    * Add files via upload
    
    * Popov solution
    
    * Add files via upload
    
    * new solutions + update

--- task3/Popov_3_1.sql
+++ task3/Popov_3_1.sql
@@ -1,3 +1,11 @@
+/*
+	Задача сформулирована недостаточно понятно:
+	ТЕКУЩЕЕ состояние клиентов - это что? Их состояние на момент выполнения запроса или в последней ревизии таблицы?
+	ТЕКУЩИЙ момент времени - это что? Момент выполнения запроса или момент времени последней ревизии таблицы?
+	Я предлагаю один вариант при трактовке обоих сущностей как момент НАЧАЛА выполнения запроса (NOW())
+	Чтобы получить решения для других вариантов, нужно поменять условия в клаузах WHERE на равенство зарезервированной дате
+*/
+
 SELECT
 	customer_rk,
 	CONCAT(current_customers.last_nm, ' ', current_customers.first_nm, ' ', current_customers.middle_nm) AS full_nm,
@@ -8,7 +16,8 @@ FROM (
 	FROM
 		srcdt.cd_customers
 	WHERE
-		NOW() BETWEEN valid_from_dttm AND valid_to_dttm) AS current_customers		
+		NOW() BETWEEN valid_from_dttm AND valid_to_dttm
+	) AS current_customers		
 LEFT JOIN (
     SELECT
 		last_nm,
@@ -24,8 +33,8 @@ LEFT JOIN (
 		first_nm,
 		middle_nm
 	) AS name_amounts
-	ON
-		current_customers.last_nm = name_amounts.last_nm
-		AND current_customers.first_nm = name_amounts.first_nm
-		AND current_customers.middle_nm = name_amounts.middle_nm
+ON
+	current_customers.last_nm = name_amounts.last_nm
+	AND current_customers.first_nm = name_amounts.first_nm
+	AND current_customers.middle_nm = name_amounts.middle_nm
 ;
