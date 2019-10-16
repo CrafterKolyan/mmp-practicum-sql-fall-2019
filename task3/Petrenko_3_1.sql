03355fa235752fb9abd010f9b8a36059148b01ad
daria-petrenko <47711608+daria-petrenko@users.noreply.github.com>
2019-10-16 15:17:38 +0300

    Petrenko 3_1 changed

--- task3/Petrenko_3_1.sql
+++ task3/Petrenko_3_1.sql
@@ -2,7 +2,7 @@ SELECT
     customer_rk, customers.nm, nm_number
 FROM
     (SELECT 
-        CONCAT(last_nm, ' ', middle_nm, ' ', first_nm) AS nm,
+        CONCAT(last_nm, ' ', first_nm, ' ', middle_nm) AS nm,
             customer_rk
     FROM
         srcdt.cd_customers
@@ -10,7 +10,7 @@ FROM
         valid_to_dttm = '5999:01:01 00:00:00') customers
         INNER JOIN
     (SELECT 
-        CONCAT(last_nm, ' ', middle_nm, ' ', first_nm) AS nm,
+        CONCAT(last_nm, ' ', first_nm, ' ', middle_nm) AS nm,
             COUNT(*) AS nm_number
     FROM
         srcdt.cd_customers
