f26223b4965c39285eaa654d3496900b973155e3
Alexander Chernyshev <32341040+sasasagagaga@users.noreply.github.com>
2019-10-16 15:34:40 +0300

    fix 3.3 (#122)
    * WIP: test solutions
    
    * WIP: test solutions
    
    * Task 03 solved
    
    * tmp
    
    * Update test_solutions.py
    
    * 3.3 fixed
    
    * Delete test_solutions.py
    
    * fix 3.3

--- task3/Chernyshev_3_3.sql
+++ task3/Chernyshev_3_3.sql
@@ -1,5 +1,5 @@
 SELECT
-    YEAR(t1.renewed_dt) AS year
+    YEAR(ap.renewed_dt) AS year,
     SUM(ap.account_renewal_cnt = 1) AS new,
     SUM(ap.account_renewal_cnt > 1) AS prolong,
     SUM(1.5 * prev_ap.opening_amt < ap.opening_amt) AS big_prolong
@@ -11,4 +11,4 @@ ON
     prev_ap.valid_to_dttm = ap.valid_from_dttm AND
     prev_ap.account_rk = ap.account_rk
 GROUP BY
-    YEAR(t1.renewed_dt);
+    YEAR(ap.renewed_dt);
