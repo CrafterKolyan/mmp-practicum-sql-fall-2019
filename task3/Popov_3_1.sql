e361386fd49d4a3e9e187b20c7f5d1625e0c314f
aredosbyk <43991422+aredosbyk@users.noreply.github.com>
2019-10-16 17:09:39 +0300

    It's not logic, but formalism (#127)
    * Add files via upload
    
    * Add files via upload
    
    * Popov solution
    
    * Add files via upload
    
    * new solutions + update
    
    * It's not logic, but formalism

--- task3/Popov_3_1.sql
+++ task3/Popov_3_1.sql
@@ -1,11 +1,3 @@
-/*
-	Задача сформулирована недостаточно понятно:
-	ТЕКУЩЕЕ состояние клиентов - это что? Их состояние на момент выполнения запроса или в последней ревизии таблицы?
-	ТЕКУЩИЙ момент времени - это что? Момент выполнения запроса или момент времени последней ревизии таблицы?
-	Я предлагаю один вариант при трактовке обоих сущностей как момент НАЧАЛА выполнения запроса (NOW())
-	Чтобы получить решения для других вариантов, нужно поменять условия в клаузах WHERE на равенство зарезервированной дате
-*/
-
 SELECT
 	customer_rk,
 	CONCAT(current_customers.last_nm, ' ', current_customers.first_nm, ' ', current_customers.middle_nm) AS full_nm,
@@ -16,7 +8,7 @@ FROM (
 	FROM
 		srcdt.cd_customers
 	WHERE
-		NOW() BETWEEN valid_from_dttm AND valid_to_dttm
+		valid_to_dttm = '5999-01-01 00:00:00'
 	) AS current_customers		
 LEFT JOIN (
     SELECT
@@ -27,7 +19,7 @@ LEFT JOIN (
 	FROM
 		srcdt.cd_customers
 	WHERE
-		NOW() BETWEEN valid_from_dttm AND valid_to_dttm
+		valid_to_dttm = '5999-01-01 00:00:00'
 	GROUP BY 
 		last_nm,
 		first_nm,
