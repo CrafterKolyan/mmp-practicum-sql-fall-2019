1efc9d3e08f4e8ff5e2a1b1eb4b452cbe4ff3d14
Svetlana Kryzhanovskaya <47041067+kryzhanovskaya@users.noreply.github.com>
2019-10-16 14:57:32 +0300

    Update  (#114)
    * Solved task 1
    
    * Add files via upload
    
    * Update Kryzhanovskaya_2_2.sql
    
    * Add files via upload
    
    * Update Kryzhanovskaya_3_2.sql
    
    * Update Kryzhanovskaya_3_2.sql
    
    * Update Kryzhanovskaya_3_3.sql

--- task3/Kryzhanovskaya_3_3.sql
+++ task3/Kryzhanovskaya_3_3.sql
@@ -1,14 +1,14 @@
-SELECT a.renewed_year, cnt_opens, cnt_prolong, IFNULL(cnt_increased, 0) as cnt_increased
+SELECT a.renewed_year, cnt_opens, cnt_prolong, IFNULL(cnt_increased, 0) AS cnt_increased
 FROM(
 	SELECT 
-		YEAR(renewed_dt) as renewed_year, 
-		SUM(account_renewal_cnt = 1) as cnt_opens, 
-		SUM(account_renewal_cnt >= 2) as cnt_prolong
+		YEAR(renewed_dt) AS renewed_year, 
+		SUM(account_renewal_cnt = 1) AS cnt_opens, 
+		SUM(account_renewal_cnt >= 2) AS cnt_prolong
 	FROM account_periods
 	GROUP BY renewed_year
 ) a
 LEFT JOIN (
-	SELECT YEAR(a.renewed_dt) as renewed_year, COUNT(*) as cnt_increased
+	SELECT YEAR(a.renewed_dt) AS renewed_year, COUNT(*) AS cnt_increased
 	FROM account_periods a
 	INNER JOIN account_periods b
 	ON 
