0ec5f6ad8b32a2ab38f1d439434028c12cd2a10c
Nikolay Korolev <CrafterKolyan@mail.ru>
2019-10-23 01:31:30 +0300

    Update Korolev_4_1.sql

--- task4/Korolev_4_1.sql
+++ task4/Korolev_4_1.sql
@@ -4,13 +4,6 @@ FROM
   account_periods
 UNION
 SELECT
-  periods1.expiration_dt AS `date`
-FROM
-  account_periods AS periods1
-  JOIN account_periods AS periods2
-  ON periods1.account_rk = periods2.account_rk AND periods1.expiration_dt = periods2.renewed_dt
-UNION
-SELECT
   expiration_dt AS `date`
 FROM
   account_periods
