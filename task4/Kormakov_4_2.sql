f983c478426a4f2acc4e4f45c4c093cce51a40be
Georgiy Kormakov <33712171+mrph2898@users.noreply.github.com>
2019-10-22 15:33:50 +0300

    Create Kormakov_4_2.sql (#136)
    * Create Kormakov_1_2.sql
    
    * Update Kormakov_1_1.sql
    
    First task is completed
    
    * Update Kormakov_1_2.sql
    
    Second task is solved
    
    * Add Kormakov_1_3.sql
    
    Task1 is completed
    
    * upper is'n needed
    
    * Solved 2 task by Kormakov
    
    Solved 2 task
    
    * Update Kormakov_2_2.sql
    
    * Create Kormakov_3_1.sql
    
    Add Kormakov_3_1.sql
    
    * Create Kormakov_3_2.sql
    
    Add Kormakov_3_2.sql
    
    * Fix some buggs in Kormakov_3_2.sql
    
    FIx Kormakov_3_2.sql
    
    * Create Kormakov_3_3.sql
    
    * Update Kormakov_3_2.sql
    
    * Create Kormakov_4_1.sql
    
    * Update Kormakov_4_1.sql
    
    * Update Kormakov_4_1.sql
    
    * Create Kormakov_4_2.sql

---
+++ task4/Kormakov_4_2.sql
@@ -0,0 +1,47 @@
+SELECT
+	customer_rk,
+    last_nm,
+    first_nm,
+    middle_nm
+FROM 
+	srcdt.cd_customers
+WHERE 
+	valid_to_dttm = '5999-01-01 00:00:00' 
+    AND customer_rk IN (
+		SELECT DISTINCT 
+			closed.customer_rk 
+		FROM (
+			SELECT
+				customer_rk,
+				account_rk
+			FROM srcdt.account_periods
+			GROUP BY customer_rk, account_rk
+			HAVING YEAR(MAX(expiration_dt)) = 2011
+		) AS closed 
+		LEFT JOIN (
+			SELECT DISTINCT
+				accounts.customer_rk,
+				accounts.account_rk 
+			FROM 
+				srcdt.account_periods AS acc
+            JOIN (
+				SELECT
+					customer_rk,
+					account_rk,
+					MAX(expiration_dt) AS date_of_closing
+				FROM 
+					srcdt.account_periods
+				GROUP BY 
+					customer_rk, account_rk
+				HAVING YEAR(date_of_closing) = 2011
+			) AS accounts
+			ON
+				accounts.customer_rk = acc.customer_rk AND
+				accounts.account_rk != acc.account_rk AND
+				accounts.date_of_closing BETWEEN acc.renewed_dt AND acc.expiration_dt
+		) AS not_closed
+		ON closed.customer_rk = not_closed.customer_rk AND
+			closed.account_rk = not_closed.account_rk
+		WHERE
+			not_closed.account_rk IS NULL
+);
