7a0d144e1b8c27fc69c185e1ca6e9cd02fec5ad3
aredosbyk <43991422+aredosbyk@users.noreply.github.com>
2019-10-23 21:58:53 +0300

    4_1

---
+++ task4/Popov_4_1.sql
@@ -0,0 +1,13 @@
+SELECT
+	renewed_dt AS possibly_renewed_dt
+FROM
+    srcdt.account_periods
+UNION
+SELECT
+	expiration_dt AS possibly_renewed_dt
+FROM
+	srcdt.account_periods
+WHERE
+	valid_to_dttm = '5999-01-01 00:00:00'
+ORDER BY renewed_dt ASC
+LIMIT 5
