b767ee19b968ac491fe7445e39a7bbd66fbaef46
ogoldobina <48651496+ogoldobina@users.noreply.github.com>
2019-10-23 15:24:58 +0300

    Update Goldobina_4_2.sql (#161)

--- task4/Goldobina_4_2.sql
+++ task4/Goldobina_4_2.sql
@@ -4,12 +4,19 @@ SELECT DISTINCT
     customers.first_nm,
     customers.middle_nm
 FROM srcdt.cd_customers AS customers
-INNER JOIN (
-    SELECT t1.customer_rk,
-           t1.account_rk,
-           t1.date_closed,
-           max(t2.expiration_dt) AS all_acc_closed
+WHERE customers.valid_to_dttm = '5999-01-01 00:00:00' AND
+      customers.customer_rk in (
+    SELECT t1.customer_rk
     FROM (
+            SELECT
+                customer_rk,
+                account_rk,
+                max(expiration_dt) AS date_closed
+            FROM srcdt.account_periods
+            GROUP BY customer_rk, account_rk
+            HAVING year(max(expiration_dt)) = 2011
+        ) AS t1
+    LEFT JOIN (
             SELECT
                 customer_rk,
                 account_rk,
@@ -17,14 +24,9 @@ INNER JOIN (
                 min(renewed_dt)    AS date_opened
             FROM srcdt.account_periods
             GROUP BY customer_rk, account_rk
-            HAVING year(max(expiration_dt)) = 2011
-        ) AS t1
-    INNER JOIN srcdt.account_periods AS t2
-    ON t1.account_rk != t2.account_rk AND
-       t1.customer_rk = t2.customer_rk AND
-       t2.renewed_dt <= t1.date_closed
-    GROUP BY t1.customer_rk, t1.account_rk, t1.date_closed
-    HAVING all_acc_closed <= t1.date_closed
-) AS closed_all_acc
-ON customers.customer_rk = closed_all_acc.customer_rk AND
-   customers.valid_to_dttm = '5999-01-01 00:00:00'
+        ) AS t2
+    ON t1.customer_rk = t2.customer_rk AND
+       t2.date_opened <= t1.date_closed AND
+       t2.date_closed > t1.date_closed
+    where t2.customer_rk is NULL
+)
