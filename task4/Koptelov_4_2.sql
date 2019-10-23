ed7e94fbcdf5fa1d962a1cc1eb1be2680f11cad6
Dmitry Koptelov <dimitar98@list.ru>
2019-10-23 11:31:02 +0300

    changed 2

--- task4/Koptelov_4_2.sql
+++ task4/Koptelov_4_2.sql
@@ -5,25 +5,23 @@ FROM
 WHERE
     valid_to_dttm = '5999-01-01 00:00:00'
         AND customer_rk IN (SELECT 
-            filtered_deposits.customer_rk
+            finished_deposits.customer_rk
         FROM
             (SELECT 
-                account_rk,
-                    ANY_VALUE(customer_rk) AS customer_rk,
-                    MAX(expiration_dt) AS close_dt
+                account_rk, customer_rk, MAX(expiration_dt) AS close_dt
             FROM
                 account_periods
-            GROUP BY account_rk
-            HAVING YEAR(close_dt) = 2011) AS filtered_deposits
+            GROUP BY customer_rk , account_rk
+            HAVING YEAR(close_dt) = 2011) finished_deposits
                 LEFT JOIN
             (SELECT 
-                ANY_VALUE(customer_rk) AS customer_rk,
+                customer_rk,
                     MIN(renewed_dt) AS open_dt,
                     MAX(expiration_dt) AS close_dt
             FROM
                 account_periods
-            GROUP BY account_rk) AS deposits ON filtered_deposits.customer_rk = deposits.customer_rk
-                AND deposits.open_dt < filtered_deposits.close_dt
-                AND filtered_deposits.close_dt < deposits.close_dt
+            GROUP BY customer_rk , account_rk) all_deposits ON finished_deposits.customer_rk = all_deposits.customer_rk
+                AND all_deposits.open_dt < finished_deposits.close_dt
+                AND finished_deposits.close_dt < all_deposits.close_dt
         WHERE
-            deposits.customer_rk IS NULL)
+            all_deposits.customer_rk IS NULL)
