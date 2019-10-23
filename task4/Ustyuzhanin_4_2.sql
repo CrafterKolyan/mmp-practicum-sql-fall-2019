924648751d70a1c48f2c53e3524a8144bd7ac9e9
alexust98 <45120679+alexust98@users.noreply.github.com>
2019-10-23 16:57:38 +0300

    н ы а

--- task4/Ustyuzhanin_4_2.sql
+++ task4/Ustyuzhanin_4_2.sql
@@ -16,18 +16,21 @@ WHERE
 		WHERE
 			YEAR(expiration_dt) = 2011
 		GROUP BY
-			customer_rk, account_rk
+			account_rk, customer_rk
 	) as tb1
 
 	LEFT JOIN
 
 	(
 		SELECT
-			account_rk, MIN(renewed_dt) as left_bound, MAX(expiration_dt) as right_bound
+			customer_rk, MIN(renewed_dt) as left_bound, MAX(expiration_dt) as right_bound
 		FROM
 			account_periods
 		GROUP BY
-			account_rk
+			account_rk, customer_rk
 	) as tb2
-	ON tb1.account_rk = tb2.account_rk AND tb2.left_bound < tb1.dt AND tb1.dt < tb2.right_bound AND tb1.customer_rk = NULL
+	ON tb1.customer_rk = tb2.customer_rk AND tb1.dt > tb2.left_bound AND tb1.dt < tb2.right_bound
+    WHERE
+		tb2.customer_rk is NULL
+    
 ) AND valid_to_dttm = '5999-01-01 00:00:00'
