8d648c74ba5744cc653e01de66b255a090bb2799
aredosbyk <43991422+aredosbyk@users.noreply.github.com>
2019-10-23 22:59:18 +0300

    4_1 4_2 (#175)
    * 4_1 4_2
    
    * 4_2

--- task4/Popov_4_2.sql
+++ task4/Popov_4_2.sql
@@ -1,6 +1,9 @@
 SELECT
 	customer_rk,
-	concat(last_nm, ' ', first_nm, ' ', middle_nm) AS full_nm
+--	concat(last_nm, ' ', first_nm, ' ', middle_nm) AS full_nm
+	last_nm,
+    first_nm,
+    middle_nm
 FROM srcdt.cd_customers
 WHERE valid_to_dttm = '5999-01-01 00:00:00'
 	AND customer_rk NOT IN (
@@ -24,6 +27,6 @@ WHERE valid_to_dttm = '5999-01-01 00:00:00'
 		GROUP BY account_rk
 	) as all_deposits
 	ON closed_in_2011.customer_rk = all_deposits.customer_rk
-		AND closed_in_2011.close_dt > deposits.open_dt 
-		AND closed_in_2011.close_dt < deposits.close_dt
+		AND closed_in_2011.close_dt > all_deposits.open_dt 
+		AND closed_in_2011.close_dt < all_deposits.close_dt
 )
