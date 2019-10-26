465635919e736916a804b5066dc18887472f5686
daria-petrenko <47711608+daria-petrenko@users.noreply.github.com>
2019-10-27 01:24:25 +0300

    Petrenko change task_4 (#179)
    * task 2
    
    * Delete petrenko_2_1.sql
    
    * Delete petrenko_2_2.sql
    
    * Delete petrenko_2_3.sql
    
    * Add files via upload
    
    * Delete Petrenko_2_2.sql
    
    * Petrenko 3_1 changed
    
    * Delete Petrenko_3_1.sql
    
    * task_4 solved
    
    * change 4_2
    
    * add 4_3
    
    * change 4_2, 4_3
    
    * change task_4

--- task4/Petrenko_4_2.sql
+++ task4/Petrenko_4_2.sql
@@ -1,30 +1,32 @@
 SELECT 
-    cust.customer_rk, last_nm, first_nm, middle_nm
+    customer_rk, last_nm, first_nm, middle_nm
 FROM
-    srcdt.cd_customers cust
-        INNER JOIN
-    (SELECT 
-        customer_rk, ANY_VALUE(if_null) AS if_null
-    FROM
-        (SELECT 
-        a.customer_rk AS customer_rk, b.customer_rk AS if_null
-    FROM
-        (SELECT 
-        customer_rk, MAX(expiration_dt) AS close_dt
-    FROM
-        srcdt.account_periods
-    GROUP BY customer_rk , account_rk
-    HAVING YEAR(close_dt) = 2011) a
-    LEFT JOIN (SELECT 
-        customer_rk,
-            MIN(renewed_dt) AS open_dt,
-            MAX(expiration_dt) AS close_dt
-    FROM
-        srcdt.account_periods
-    GROUP BY customer_rk , account_rk) b ON a.customer_rk = b.customer_rk
-        AND a.close_dt > b.open_dt
-        AND a.close_dt < b.close_dt) t
-    GROUP BY customer_rk) flag ON cust.customer_rk = flag.customer_rk
-        AND if_null IS NULL
+    srcdt.cd_customers
 WHERE
-    cust.valid_to_dttm = '5999-01-01 00:00:00'
+    valid_to_dttm = '5999-01-01 00:00:00'
+        AND customer_rk IN (SELECT DISTINCT
+            customer_rk
+        FROM
+            (SELECT 
+                a.customer_rk AS customer_rk, b.customer_rk AS null_flag
+            FROM
+                (SELECT 
+                account_rk,
+                    ANY_VALUE(customer_rk) AS customer_rk,
+                    MAX(expiration_dt) AS closed_dt
+            FROM
+                srcdt.account_periods
+            GROUP BY account_rk
+            HAVING YEAR(closed_dt) = 2011) a
+            LEFT JOIN (SELECT 
+                account_rk,
+                    ANY_VALUE(customer_rk) AS customer_rk,
+                    MIN(renewed_dt) AS open_dt,
+                    MAX(expiration_dt) AS closed_dt
+            FROM
+                srcdt.account_periods
+            GROUP BY account_rk) b ON a.customer_rk = b.customer_rk
+                AND a.closed_dt > b.open_dt
+                AND a.closed_dt < b.closed_dt) t
+        WHERE
+            null_flag IS NULL)
